7bfbbce321d92f9509a9d9357212f7e28a59543b
Sxxxk <794670550@qq.com>
2023-11-29 15:27:04 +0800

    Update ZYMWaveletTest.py

--- ZYMWaveletTest.py
+++ ZYMWaveletTest.py
@@ -3,36 +3,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 def haar_wavelet_3d(data):
     # 高通滤波
+    #以下为正确解值
     aaa = (data[::2, ::2, ::2] + data[1::2, ::2, ::2] + data[::2, 1::2, ::2] + data[1::2, 1::2, ::2] +
-           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / 8.0
+           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / np.sqrt(8)
 
     aad = (data[::2, ::2, ::2] + data[1::2, ::2, ::2] + data[::2, 1::2, ::2] + data[1::2, 1::2, ::2] -
-           (data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2])) / 8.0
+           (data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2])) / np.sqrt(8)
 
     ada = (data[::2, ::2, ::2] + data[1::2, ::2, ::2] - (data[::2, 1::2, ::2] + data[1::2, 1::2, ::2]) +
-           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] - (data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2])) / 8.0
+           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] - (data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2])) / np.sqrt(8)
 
-    daa = (data[::2, ::2, ::2] - (data[1::2, ::2, ::2] + data[::2, 1::2, ::2] + data[1::2, 1::2, ::2]) +
-           data[::2, ::2, 1::2] - (data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2])) / 8.0
+    daa = (data[::2, ::2, ::2] - data[1::2, ::2, ::2] + data[::2, 1::2, ::2] - data[1::2, 1::2, ::2] +
+           data[::2, ::2, 1::2] - data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] - data[1::2, 1::2, 1::2]) / np.sqrt(8)
 
     # 低通滤波
-    add = (data[::2, ::2, ::2] + data[1::2, ::2, ::2] + data[::2, 1::2, ::2] + data[1::2, 1::2, ::2] +
-           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / 8.0
-    dad = (data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / 4.0
-    dda = (data[1::2, ::2, ::2] + data[1::2, 1::2, ::2] + data[1::2, ::2, 1::2] + data[1::2, 1::2, 1::2]) / 4.0
-    ddd = (data[::2, 1::2, ::2] + data[1::2, 1::2, ::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / 4.0
+    add =  (data[::2, ::2, ::2] + data[1::2, ::2, ::2] - data[::2, 1::2, ::2] - data[1::2, 1::2, ::2] -
+            data[::2, ::2, 1::2] - data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / np.sqrt(8)
 
-    return aaa, aad, ada, daa, add, dad, dda, ddd
+    dad = (data[::2, ::2, ::2] - data[1::2, ::2, ::2] + data[::2, 1::2, ::2] - data[1::2, 1::2, ::2] -
+           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] - data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / np.sqrt(8)
+
+    dda = (data[::2, ::2, ::2] - data[1::2, ::2, ::2] - data[::2, 1::2, ::2] + data[1::2, 1::2, ::2] +
+           data[::2, ::2, 1::2] - data[1::2, ::2, 1::2] - data[::2, 1::2, 1::2] + data[1::2, 1::2, 1::2]) / np.sqrt(8)
+
+    ddd =  (data[::2, ::2, ::2] - data[1::2, ::2, ::2] - data[::2, 1::2, ::2] + data[1::2, 1::2, ::2] -
+           data[::2, ::2, 1::2] + data[1::2, ::2, 1::2] + data[::2, 1::2, 1::2] - data[1::2, 1::2, 1::2]) / np.sqrt(8)
+
+    coeffs = {'aaa': aaa, 'aad': aad, 'ada': ada, 'daa': daa, 'add': add, 'dad': dad, 'dda': dda, 'ddd': ddd}
+    return coeffs
 
 # 生成一个三维数据，这里假设是一个立方体
-data = np.random.random((8, 8, 8))
+data = np.random.random((4,4,4))
+data1 = data
 
 # 进行三维离散小波变换
 coeffs = haar_wavelet_3d(data)
+print(coeffs['ddd'])
+print("next")
 
 # 可视化一个示例，这里只展示了 add 分量
 import matplotlib.pyplot as plt
-plt.imshow(coeffs[4][:, :, 0], cmap='gray')
+plt.imshow(coeffs['ddd'][:, :, 0], cmap='gray')
 plt.title('add Component')
 plt.show()
 
@@ -43,7 +54,7 @@ plt.show()
 
 
 # 进行三维离散小波变换
-coeffs = pywt.dwtn(data, 'haar')  # 使用Haar小波
+coeffs = pywt.dwtn(data1, 'haar')  # 使用Haar小波
 
 # 逐层展示高频和低频分量
 # 高通滤波
@@ -61,7 +72,7 @@ ddd = coeffs['ddd']
 # 在这里可以对高频和低频分量进行进一步处理，或者可视化
 
 # 可视化一个示例，这里只展示了 aaa 分量
-print(dad)
-plt.imshow(dad[:, :, 0], cmap='gray')
-plt.title("add")
+print(ddd)
+plt.imshow(ddd[:, :, 0], cmap='gray')
+plt.title("dad")
 plt.show()
